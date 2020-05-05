49fb4ee573f5c59a248b577c237f3aed419e086b
EduardoRamponMeireles <eduardomeireles3232@gmail.com>
2020-05-05 17:06:51 -0300

    header: user information filled in

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import { Container, Content, Profile } from './styles';
@@ -7,6 +8,8 @@ import Notifications from '~/components/Notifications';
 import logo from '~/assets/logo-purple.svg';
 
 function Header() {
+  const profile = useSelector((state) => state.user.profile);
+
   return (
     <Container>
       <Content>
@@ -18,11 +21,14 @@ function Header() {
           <Notifications />
           <Profile>
             <div>
-              <strong>Eduardo Rampon</strong>
+              <strong>{profile.name}</strong>
               <Link to="/profile">Meu perfil</Link>
             </div>
             <img
-              src="https://api.adorable.io/avatars/50/abott@adorable.png"
+              src={
+                profile.avatar.url ||
+                'https://api.adorable.io/avatars/50/abott@adorable.png'
+              }
               alt="Avatar"
             />
           </Profile>
