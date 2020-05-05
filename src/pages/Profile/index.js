7fd5aba248914a7cc1dd269613a19c85b3fb343e
EduardoRamponMeireles <eduardomeireles3232@gmail.com>
2020-05-04 21:56:43 -0300

    update user is working (saga and reducer)

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Form, Input } from '@rocketseat/unform';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { updateProfileRequest } from '~/store/modules/user/actions';
 
 import { Container } from './styles';
 
 export default function Profile() {
   const profile = useSelector((state) => state.user.profile);
+  const dispatch = useDispatch();
 
-  function handleSubmit(data) {}
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
 
   return (
     <Container>
-      <Form initialData={profile}>
+      <Form onSubmit={handleSubmit} initialData={profile}>
         <Input name="name" placeholder="Nome Completo" />
         <Input name="email" type="email" placeholder="Seu endereço de email" />
         <hr />
